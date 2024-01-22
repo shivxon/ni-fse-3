6258938b9d0d3e8c70113652534ae26f22df84dc
Shivam Vashishtha <shivam.vashishtha@mobilecoderz.com>
2024-01-22 12:23:47 +0530

    user details page added

--- frontend/src/components/user/user-details.tsx
+++ frontend/src/components/user/user-details.tsx
@@ -1,27 +1,49 @@
 import React, { useState, useEffect } from "react";
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { AppDispatch, RootState, useAppDispatch } from "../../store/store"
 import { getUserDetails } from "../../reducers/user-details-reducer";
 import Card from 'react-bootstrap/Card';
 
-const UserDetails = ()=>{
- const params = useParams()
- console.log('params',params)
- const dispatch: AppDispatch = useAppDispatch();
- const userDetails: any = useSelector((state: RootState) => state.getUserDetails.details)
- console.log('userDetails', userDetails)
- useEffect(() => {
+const UserDetails = () => {
+  const params = useParams()
+  console.log('params', params)
+  const dispatch: AppDispatch = useAppDispatch();
+  const userDetails: any = useSelector((state: RootState) => state.getUserDetails.details)
+  console.log('userDetails', userDetails)
+  useEffect(() => {
     dispatch(getUserDetails(params?.id as string))
   }, [dispatch]);
 
-    return (<div>
-      
-      Details Page
-      <Card style={{ width: '40rem', height: '25rem', padding: '40px' }}>
-      
-      </Card>
-      </div>);
+  return (<div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '40px',
+
+  }}>
+    <Card style={{ width: '40rem', height: '25rem', padding: '40px' }}>
+     <div style={{display:'flex', justifyContent:'center'}}> <h4>User Details</h4> </div>
+      <div >
+        <ul>
+          <li style={{display:'flex', justifyContent:'flex-start'}}><span>First Name :</span>
+            <p style={{marginLeft:'10px'}}>
+              {userDetails?.data?.firstName}
+            </p>
+          </li>
+          <li  style={{display:'flex', justifyContent:'flex-start'}}><span>Last Name :</span>
+            <p style={{marginLeft:'10px'}}>
+              {userDetails?.data?.lastName}
+            </p>
+          </li>
+          <li  style={{display:'flex', justifyContent:'flex-start'}}><span>Phone Number :</span>
+            <p style={{marginLeft:'10px'}}>
+              {userDetails?.data?.phone}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </Card>
+  </div>);
 }
 
 export default UserDetails
